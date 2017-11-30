1f0821c316689b909d93775ba05da8dd3fcbd8dd
KarelAlofs <r0624198@student.thomasmore.be>
2017-11-30 15:55:08 +0100

    Whatever

--- app/src/main/java/be/thomasmore/underground/rest/APIInterface.java
+++ app/src/main/java/be/thomasmore/underground/rest/APIInterface.java
@@ -1,10 +1,18 @@
 package be.thomasmore.underground.rest;
 
+import java.util.List;
+
+import be.thomasmore.underground.classes.Terrorist;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 public interface APIInterface {
+
+    @GET("/api/terrorists")
+    Call<List<Terrorist>> getTerrorists(@Header("Authorization") String accessToken );
 
     @POST("/api/auth/login")
     Call createUser(@Body String qrCode);
